16742e1690999eb59df555c3771fa33bd2f98adc
simonewebdesign <hello@simonewebdesign.it>
2013-02-06 14:59:28 -0800

    Add link to stylesheet and remove H1

--- application/views/layouts/application.blade.php
+++ application/views/layouts/application.blade.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <title>Laravel-REST</title>
+  {{ HTML::style("css/scaffolds.css") }}
 </head>
 <body>
-  <h1>Laravel-REST</h1>
-  
+
   @if ( Session::get('notice') )
   <p id="notice">{{ Session::get('notice') }}</p>
   @endif
